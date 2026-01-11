f88cd5296deb8bd4713e6de90ffc37ae6958d0a7
ariff <23002125@siswa.um.edu.my>
2026-01-11 22:33:04 +0800

    comitting my fragment_indian_instrument.xml and the drawables

--- app/src/main/java/com/example/app4ll/IndianInstrument.java
+++ app/src/main/java/com/example/app4ll/IndianInstrument.java
@@ -1,5 +1,6 @@
 package com.example.app4ll;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +16,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class IndianInstrument extends Fragment {
+
+    Button btnSitar;
+
+    MediaPlayer mp;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -49,9 +55,12 @@ public class IndianInstrument extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -61,4 +70,57 @@ public class IndianInstrument extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_indian_instrument, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initialize button here after view is created
+        btnSitar = view.findViewById(R.id.btnSitarPlay);
+        Button btnTambura = view.findViewById(R.id.btnTamburaPlay);
+        Button btnTabla = view.findViewById(R.id.btnTablaPlay);
+        Button btnMridangam = view.findViewById(R.id.btnmridangamPlay);
+        Button btnBansuri = view.findViewById(R.id.btnBansuriPlay);
+        Button btnNadaswaram = view.findViewById(R.id.btnNadaswaramPlay);
+        Button btnGhatam = view.findViewById(R.id.btnGhatamPlay);
+        Button btnTaal = view.findViewById(R.id.btnTaalPlay);
+
+        btnSitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSound(R.raw.sitar);
+            }
+        });
+
+        btnTambura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSound(R.raw.tambura);
+            }
+        });
+    }
+
+    private void playSound(int soundResourceId) {
+        // Release previous MediaPlayer if it exists
+        if (mp != null) {
+            mp.release();
+            mp = null;
+        }
+        mp = MediaPlayer.create(requireContext(), soundResourceId);
+
+        if (mp != null) {
+            mp.start();
+
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.release();
+                    mp = null;
+                }
+            });
+        }
+    }
+
+
+
 }
