cb83356f9fde9087a48f10029969bc215795f293
ariff <23002125@siswa.um.edu.my>
2026-01-13 17:41:04 +0800

    comitting bottom navigation view india

--- app/src/main/java/com/example/app4ll/IndianActivity.java
+++ app/src/main/java/com/example/app4ll/IndianActivity.java
@@ -37,7 +37,7 @@ public class IndianActivity extends AppCompatActivity {
             } else if (itemId == R.id.indianfood) { // Corrected the menu item ID from DestChinaFood
                 selectedFragment = new IndianFood();
             } else if (itemId == R.id.indianhome) {
-                //selectedFragment = new IndianFragment();
+                selectedFragment = new IndianFragment();
             }
 
             if (selectedFragment != null) {
@@ -50,10 +50,10 @@ public class IndianActivity extends AppCompatActivity {
 
         // Load ChineseFragment by default on startup
         if (savedInstanceState == null) {
-            /*getSupportFragmentManager().beginTransaction()
+            getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container_indian, new IndianFragment())
                     .commit();
-            bottomNav.setSelectedItemId(R.id.indianhome);*/
+            bottomNav.setSelectedItemId(R.id.indianhome);
         }
 
         // Removed the incorrect private inner class definition
